b8b2cfed1cc96319ba29b23ae834ec6729fe1d4f
Vsevolod Reshetnikov <vsevolod_reshetnikov@epam.com>
2019-07-06 13:50:41 +0300

    Add many to many relation

--- src/main/java/night/clubs/data/NightClub.java
+++ src/main/java/night/clubs/data/NightClub.java
@@ -1,15 +1,29 @@
 package night.clubs.data;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import java.util.List;
 
+@Entity
 public class NightClub {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
+    
     private String name;
+
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(
+            name = "club_visit",
+            joinColumns = { @JoinColumn(name = "club_id") },
+            inverseJoinColumns = { @JoinColumn(name = "visitor_id") }
+    )
     private List<Visitor> visitors;
 
     public NightClub(String name) {
--- src/main/java/night/clubs/data/Visitor.java
+++ src/main/java/night/clubs/data/Visitor.java
@@ -1,15 +1,21 @@
 package night.clubs.data;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import java.util.List;
 
+@Entity
 public class Visitor {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
+    
     private String name;
+
+    @ManyToMany(mappedBy = "visitors")
     private List<NightClub> clubsVisited;
 
     public Visitor(String name) {
