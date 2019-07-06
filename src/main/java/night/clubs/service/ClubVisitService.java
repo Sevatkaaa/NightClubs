637ecaf6c42677063fb04b4b72f896b51ad8cdb8
Vsevolod Reshetnikov <vsevolod_reshetnikov@epam.com>
2019-07-06 18:06:01 +0300

    Remove unneeded field

--- src/main/java/night/clubs/service/ClubVisitService.java
+++ src/main/java/night/clubs/service/ClubVisitService.java
@@ -3,7 +3,6 @@ package night.clubs.service;
 import night.clubs.model.NightClub;
 import night.clubs.model.Visitor;
 import night.clubs.repository.NightClubRepository;
-import night.clubs.repository.VisitorRepository;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -13,9 +12,6 @@ import java.util.List;
 
 @Service
 public class ClubVisitService {
-    @Resource
-    private VisitorRepository visitorRepository;
-    
     @Resource
     private NightClubRepository nightClubRepository;
 
