0f3addcca00f4e84f68c209c3ac934a5e6cb2e7d
vsevolod.reshetnikov <h2g5oxkldrnsn5knivgy2tyxtufm2n4gbbbevwlbtmxhscko366a>
2022-01-06 23:54:09 +0200

    fix mysql for 8+ version

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -2,3 +2,4 @@ CREATE DATABASE nightclubs;
 CREATE USER 'nightclubuser'@'localhost' IDENTIFIED BY 'club';
 GRANT ALL PRIVILEGES ON nightclubs.* TO 'nightclubuser'@'localhost';
 FLUSH PRIVILEGES;
+ALTER USER 'nightclubuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'club';
